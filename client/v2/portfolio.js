9133278af3072a1c7a7563ccf368a23d4d3b09f9
Antoine Cohen <antoine.cohen@edu.devinci.fr>
2022-02-06 10:40:11 +0100

    feat(browse_pages) + added buttons feat(favorites)

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -3,16 +3,19 @@
 
 // current products on the page
 let currentProducts = [];
+let favoriteProducts = [];
 let currentPagination = {};
 
 // inititiate selectors
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
 const sectionProducts = document.querySelector('#products');
+const sectionFavoriteProducts = document.querySelector('#products_favorite');
 const spanNbProductsTotal = document.querySelector('#nbProductsTotal');
 const spanNbProducts = document.querySelector('#nbProducts');
 const spanNbNewProducts = document.querySelector('#nbNewProducts');
 const spanP90 = document.querySelector('#p90');
+// const button_favorites = document.querySelector('#add_favorites');
 
 /**
  * Set global value
@@ -41,7 +44,7 @@ const fetchProducts = async (page = 1, size = 12) => {
       console.error(body);
       return {currentProducts, currentPagination};
     }
-
+    console.log('data returned:'+body.data.meta);
     return body.data;
   } catch (error) {
     console.error(error);
@@ -53,6 +56,34 @@ const fetchProducts = async (page = 1, size = 12) => {
  * Render list of products
  * @param  {Array} products
  */
+const renderFavorites = products_favorites =>{
+  // renderProducts(currentProducts);
+
+  currentProducts.forEach(product => {
+    const button_fav = document.querySelector("#add_favorites "+product.name)
+    console.log(button_fav);
+  })
+  
+  const fragment = document.createDocumentFragment();
+  const div = document.createElement('div');
+  const template = products_favorites
+  .map(product => {
+    return `
+    <div class="product" id=${product.uuid}>
+      <span>${product.brand}</span>
+      <a href="${product.link}">${product.name}</a>
+      <span>${product.price}</span>
+    </div>
+  `;
+  })
+  .join('');
+  div.innerHTML = template;
+  fragment.appendChild(div);
+  sectionFavoriteProducts.innerHTML = '<h2>Favorite products</h2>';
+  sectionFavoriteProducts.appendChild(fragment);
+  
+
+}
 
 const renderProducts = products => {
   const fragment = document.createDocumentFragment();
@@ -64,6 +95,10 @@ const renderProducts = products => {
         <span>${product.brand}</span>
         <a href="${product.link}">${product.name}</a>
         <span>${product.price}</span>
+        <button id="add_favorites ${product.name}"
+        type="button">
+          Add to favorites
+        </button>
       </div>
     `;
     })
@@ -73,6 +108,7 @@ const renderProducts = products => {
   fragment.appendChild(div);
   sectionProducts.innerHTML = '<h2>Products</h2>';
   sectionProducts.appendChild(fragment);
+  
 };
 
 
@@ -120,25 +156,23 @@ const renderNewProducts = products => {
 
 const renderP90 = products => {
   var idx = parseInt(products.length*0.9);
-  var sorted = SortAsc
+  // var sorted = SortAsc
 }
 
 const render = (products, pagination) => {
   renderProducts(products);
+  // renderFavorites(favoriteProducts);
   renderPagination(pagination);
   renderIndicators(pagination);
   renderNbProducts(products);
   renderNewProducts(products);
   renderP90(products);
+  // const button_favorites = document.querySelector('#add_favorites Le hoodie');
+  // console.log(button_favorites);
 };
 const selProductsByPage = (pageNumber) =>{
 
-<<<<<<< HEAD
 }
-=======
-//Test
-
->>>>>>> 5e8efc16def1041c90346c7e3ab8575a86bda789
 /**
  * Declaration of all Listeners
  */
@@ -150,6 +184,7 @@ const selProductsByPage = (pageNumber) =>{
 selectShow.addEventListener('change', event => {
   console.log('current page:',currentPagination.currentPage);
   console.log('target value:',event.target.value);
+  
   fetchProducts(currentPagination.currentPage, parseInt(event.target.value))
     .then(setCurrentProducts)
     .then(() => render(currentProducts, currentPagination));
@@ -161,15 +196,18 @@ document.addEventListener('DOMContentLoaded', () =>
     .then(() => render(currentProducts, currentPagination))
 );
 
-selectPage.addEventListener('change',event =>{
-  // fetchProducts(page a changer,)
-  currentPagination.currentPage = event.target.value;
-  console.log('target value:',event.target.value);
-  console.log('page size:',currentPagination.pageSize);
-  fetchProducts(currentPagination.currentPage,currentPagination.pageSize).
+selectPage.addEventListener('change',(event) =>{
+  var e = document.getElementById("show-select");
+  var strUser = e.value;
+
+  fetchProducts(parseInt(event.target.value),parseInt(strUser)).
   then(setCurrentProducts)
   .then(() => render(currentProducts, currentPagination))
 })
+
+// button_favorites.addEventListener('click',() =>{
+//   console.log('Added to favorites!');
+// })
 
 
 
